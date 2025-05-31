5511771f5060732c87ca58f20dc29967af6f967e
Aditya Kumar <42202518+imadityagolu@users.noreply.github.com>
2025-05-31 17:01:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+const axios = require("axios");
+
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+app.use("/api/v1/jokes", async (req, res) => {
+
+    try {
+        const response = await axios.get("http://api.api-ninjas.com/v1/dadjokes", {
+            headers: {
+                "X-Api-Key": process.env.API_KEY,
+            },
+        });
+
+    if(response.data && response.data.length > 0){
+        console.log("Api is working !!")
+        res.status(200).json({
+            success: true,
+            j0ke: response.data[0].joke,
+        });
+    }
+    else{
+        console.log("No joke available")
+        res.status(404).json({
+            success: false,
+            joke: "No joke available",
+        });
+    }
+
+    } catch(error){
+    console.log("Server/Api is not working")
+    res.status(500).json({
+        status: false,
+        joke: "Server/Api is not working",
+    });
+    }
+
+})
+
+app.listen(PORT, () => { console.log(`server is running at port - http://localhost:${PORT}/api/v1/jokes`) });
